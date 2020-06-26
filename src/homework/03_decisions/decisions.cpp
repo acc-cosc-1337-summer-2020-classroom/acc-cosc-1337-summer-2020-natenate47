74634a0751922189048ce1e6784003df88cdb7cd
natenate47 <natenate47@gmail.com>
2020-06-26 02:19:51 +0000

    Tic Tac Toe HW & Assignment 2

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -43,6 +43,22 @@ int get_grade_points(std::string letter_grade)
 //credit_hours. In the function account for division by zero by returning a -1.
 double calculate_gpa(int credit_hours, double credit_points)
 {
+    if (credit_hours != 0)
+    {
+        return credit_points / credit_hours;
+    }
+    else
+    {
+        return -1;
+    }
+
+}
+
+//Write code for function calculate_gpa that accepts an int named credit_hours and
+//a double named credit_points.  The function returns the quotient of credit_points divided by
+//credit_hours. In the function account for division by zero by returning a -1.
+double calculate_gpa(int credit_hours, double credit_points)
+{
 	return credit_points / credit_hours;
 }
 
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
-#include<string>
-using std::cout; using std::cin; using std::string;
 //write include statements
 #include "decisions.h"
-
+#include<iostream>
+#include<string>
+using std::cout;
+using std::cin;
+using std::string;
 /*
 In main write code to capture a letter_grade and credit_hours from the keyboard:
-
 
 Grade   Credit Hours	grade_points   credit_points
 A          3            4              12
@@ -26,33 +26,25 @@ GPA 3.0
 */
 int main() 
 {
-	string letter_grade;
-	int credit_hours, sum_credit_hours = 0, sum_credit_points = 0;
+    // initialize variables
+    std::string letter_grade = ""; 
+    int credit_hours, sum_credit_hours = 0, sum_credit_points = 0;
+    char choice;
+    // loop for user to enter letter grade, credit hours, then choose to continue or not
+    do {
+    	std::cout << "Enter letter grade: ";
+    	std::cin >> letter_grade;
+   	 	std::cout << "Enter credit hours: ";
+    	std::cin >> credit_hours;
+    	sum_credit_points += get_grade_points(letter_grade)  * credit_hours;
+    	sum_credit_hours += credit_hours;
 
-	cout << "Enter letter grade: ";
-	cin >> letter_grade;
-	cout << "Enter credit hours: ";
-	cin >> credit_hours;
-	sum_credit_points += get_grade_points(letter_grade) * credit_hours;
-	sum_credit_hours += credit_hours;
+    	std::cout<<"\n";
 
-	cout << "Enter letter grade: ";
-	cin >> letter_grade;
-	cout << "Enter credit hours: ";
-	cin >> credit_hours;
-	sum_credit_points += get_grade_points(letter_grade)  * credit_hours;
-	sum_credit_hours += credit_hours;
+    	std::cout << "Enter y or Y to continue: ";
+    	std::cin >> choice;
 
-	cout << "Enter letter grade: ";
-	cin >> letter_grade;
-	cout << "Enter credit hours: ";
-	cin >> credit_hours;
-	sum_credit_points += get_grade_points(letter_grade)  * credit_hours;
-	sum_credit_hours += credit_hours;
-
-
-	double gpa = calculate_gpa(sum_credit_hours, sum_credit_points);
-	cout << "GPA: " << gpa;
-
-	return 0;
+    	std::cout<<"\n";
+    }
+    while (choice == 'y' || choice == 'Y');
 }
